ff2b7b0fd67f1b25291f3d9f24a33d0be7d144f4
GregoryGA <grishahtml@gmail.com>
2021-07-30 19:59:58 +0500

    Добавляет функцию числа с плавающей точкой и функцию с целым числом + исправления наставника 2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 // Результат: число с плавающей точкой из диапазона "от...до" с указанным "количеством знаков после запятой"
 //[https://developer.mozilla.org/ru/docs/orphaned/Web/JavaScript/Reference/Global_Objects/Math/random]
 
-function randomfract(min, max, number) {
+
+const randomfract = function (min, max, number) {
   if (min > max) {
     return Error();
   }
   return +(Math.random() * (max - min + 1) + min).toFixed(number);
-}
+};
 //Получение случайного целого числа в заданном интервале, включительно
-function randomint(min, max) {
+const randomint = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
+randomfract();
+randomint();
